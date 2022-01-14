4e54a2ba3aed63245472078b5d164b123b7a89cb
Luijma <rsluismanuel@gmail.com>
2022-01-14 00:27:51 +0000

    betty issues

--- M-error_handler_2.c
+++ M-error_handler_2.c
@@ -7,6 +7,10 @@ void pop_error(unsigned int n)
 {
 	fprintf(stderr, "L<%d>: can't pop an empty stack\n", n);
 }
+/**
+ * swap_error - handles error with swap usage
+ * @n: line of error
+ */
 void swap_error(unsigned int n)
 {
 	fprintf(stderr, "L<%d>: can't swap, stack too short\n", n);
